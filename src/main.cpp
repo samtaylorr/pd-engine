bfb9b0424ec049b46e25fb13ea7a411df41dddb2
samtaylorr <U1963266@unimail.hud.ac.uk>
2023-06-30 02:06:18 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <glfw/glfw3.h>
+// Main() function: where the execution of
+// program begins
+int main()
+{
+	// Prints hello world
+  std::cout << "Hello World";
+
+	return 0;
+}
