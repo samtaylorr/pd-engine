5db1a4c28b6b687c4d3dd1486d1c8f52eabab9c4
samtaylorr <U1963266@unimail.hud.ac.uk>
2023-06-30 20:08:17 +0100

    Added shader files

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,122 @@
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "pd-engine/Shader.h"
+#include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-      glViewport(0, 0, width, height);
-}  
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
-void processInput(GLFWwindow *window)
-{
-  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-    glfwSetWindowShouldClose(window, true);
-  }
-}
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-// Main() function: where the execution of
-// program begins
 int main()
 {
+  // glfw: initialize and configure
+  // ------------------------------
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  
-  GLFWwindow* window = glfwCreateWindow(800, 600, "p-engine", NULL, NULL);
+
+  #ifdef __APPLE__
+      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  #endif
+
+  // glfw window creation
+  // --------------------
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "PD Engine", NULL, NULL);
   if (window == NULL)
   {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
-
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
 
-  glViewport(0, 0, 800, 600);  
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // build and compile our shader program
+  // ------------------------------------
+  Shader ourShader("../shaders/example/basic/vertex.glsl", "../shaders/example/basic/fragment.glsl");
 
-  while(!glfwWindowShouldClose(window))
-  {
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  float vertices[] = {
+    //positions                           //colors
+    0.5f, -0.5f, 0.0f, /*top*/            1.0f, 0.0f, 0.0f, // red
+   -0.5f, -0.5f, 0.0f, /*bottom right*/   0.0f, 1.0f, 0.0f, // green
+    0.0f, 0.5f, 0.0f,  /* bottom left*/   1.0f, 0.0f, 1.0f  // blue
+  };
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  // bind the VAO, bind/set vert buffers, configure vert attr
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(0);
+
+  // wireframe mode:
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  // render loop
+  while(!glfwWindowShouldClose(window)){
+    // input 
+    // -----
     processInput(window);
 
-    //rendering commands go here
+    // render
+    // ------
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // draw our triangle
+    ourShader.use();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(0); // no need to unbind it every time 
+    
+    // glfw: swap buffers/poll IO event (we need to do this for input detection)
     glfwSwapBuffers(window);
-    glfwPollEvents();    
+    glfwPollEvents();
   }
 
+  // deallocation
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+
+  // glfw: terminate, clear allocated resources
   glfwTerminate();
   return 0;
+}
+
+// process input void 
+void processInput(GLFWwindow *window)
+{
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whether window size changed
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0,0,width,height);
 }
